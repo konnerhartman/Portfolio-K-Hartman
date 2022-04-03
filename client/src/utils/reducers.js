b7353a68203129355388f460b6cfea75bb265573
Konner Hartman <konner.hartman@yahoo.com>
2022-04-03 11:30:16 -0500

    added reducers to utils

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,21 @@
+import { useReducer } from 'react';
+import {
+  UPDATE_PROJECTS,
+} from './actions';
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case UPDATE_PROJECTS:
+      return {
+        ...state,
+        projects: [...action.projects],
+      };
+
+    default:
+      return state;
+  }
+};
+
+export function useProjectReducer(initialState) {
+  return useReducer(reducer, initialState);
+}
