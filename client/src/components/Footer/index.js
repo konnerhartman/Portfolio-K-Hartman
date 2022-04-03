e3ab06f418b6d499d889e70e283d90e8ffa5b636
Konner Hartman <konner.hartman@yahoo.com>
2022-04-03 11:08:28 -0500

    added code to footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+// import KHLogo from "../../assets/KH-LOGO-sm.png"
+
+function Footer() {
+return (
+  <div>
+    <footer className="text-white sticky-bottom bg-dark">
+      <div className="row px-2 pt-3 row text-center">
+        <p className="col-lg-4 col-sm-12 p-2">© 2022 Copyright</p>
+        <div className="col-lg-2 d-none d-sm-block"></div>
+        <div className="col-lg-2 d-none d-sm-block"></div>
+        <p className="col-lg-4 col-sm-12 p-2">Website built by:
+          {/* <a className="text-decoration-none p-2" href="https://github.com/konnerhartman">
+            <img height="18rem" src={KHLogo} alt="Konner Hartman Logo" />
+          </a> */}
+        </p>
+      </div>
+      <div className="">
+        
+      </div>
+    </footer>
+  </div>
+  
+)
+};
+
+export default Footer;
