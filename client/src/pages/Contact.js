fcc7a6ac5a6c0e3fded461cb948dbb9d3cc889de
Konner Hartman <konner.hartman@yahoo.com>
2022-04-07 12:24:35 -0500

    styled contact page a little more

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -3,7 +3,7 @@ import emailjs from '@emailjs/browser';
 import ProfPic from '../assets/Prof-Pic-Square.jpg'
 import resume from '../assets/K.-Hartman-Resume.pdf';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPaperPlane, faMobileAlt, faEnvelope  } from '@fortawesome/free-solid-svg-icons';
+import { faPaperPlane, faMobileAlt, faFileInvoice  } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faGithubAlt } from '@fortawesome/free-brands-svg-icons';
 
 const Contact = () => {
@@ -25,64 +25,57 @@ const Contact = () => {
     <div className="row">
         <div className="col-1 d-none d-sm-block">
         </div>
-        <div className="glass col-lg-10 col-md-12 row">
-        <h2 className='text-center'>Contact Me</h2>
-            <div className='col-md-4'>
+        <div className="glass col-lg-10 col-md-12 row p-2">
+            <h2 className='text-center'>Reach Out</h2>
+            <div className='col-md-5 d-none d-sm-block'>
                 <img className="prof-pic rounded-circle" src={ProfPic} alt="Konner sitting infront of his desk with a big smile on his face!."/>
             </div>
-            <div className="col-md-8 p-3">
-                <form onSubmit={sendEmail} id="contact-form" name="contact-form" action="mail.php" method="POST">
-                <div className="row mb-3">
-                    <div className="col-md-6">
-                    <div className="md-form mb-0">
-                        <input type="text" id="name" name="name" className="form-control" required />
-                        <label className="">Your name</label>
-                    </div>
-                    </div>
-                    <div className="col-md-6">
-                    <div className="md-form mb-0">
-                        <input type="text" id="email" name="email" className="form-control" required />
-                        <label className="">Your email</label>
-                    </div>
-                    </div>
+            <div className="col-md-7 col-sm-12">
+                <div className='col-12 row text-center'>
+                    <a className='contact col-3' target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/konner-hartman/"><FontAwesomeIcon icon={faLinkedin} /></a>
+                    <a className='contact col-3' target="_blank" rel="noreferrer" href="https://github.com/konnerhartman"><FontAwesomeIcon icon={faGithubAlt} /></a>
+                    <a className='contact col-3' href="tel:214-226-4278"><FontAwesomeIcon icon={faMobileAlt} /></a>
+                    <a className='contact col-3' href={resume}><FontAwesomeIcon icon={faFileInvoice} /></a>
                 </div>
-                <div className="row  mb-3">
-                    <div className="col-md-12">
-                    <div className="md-form mb-0">
-                        <input type="text" id="subject" name="subject" className="form-control" required />
-                        <label className="">Subject</label>
+                <form className='py-2' onSubmit={sendEmail} id="contact-form" name="contact-form" action="mail.php" method="POST">
+                    <p className='text-center'>-Send Me An Email-</p>
+                    <div className="row mb-3">
+                        <div className="col-md-6">
+                            <div className="md-form mb-0">
+                                <input type="text" id="name" name="name" className="form-control" required />
+                                <label className="">Your name</label>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="md-form mb-0">
+                                <input type="text" id="email" name="email" className="form-control" required />
+                                <label className="">Your email</label>
+                            </div>
+                        </div>
                     </div>
+                    <div className="row  mb-3">
+                        <div className="col-md-12">
+                            <div className="md-form mb-0">
+                                <input type="text" id="subject" name="subject" className="form-control" required />
+                                <label className="">Subject</label>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-12">
-                    <div className="md-form">
-                        <textarea type="text" id="message" name="message" rows="2" className="form-control md-textarea" required></textarea>
-                        <label>Your message</label>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="md-form">
+                                <textarea type="text" id="message" name="message" rows="2" className="form-control md-textarea" required></textarea>
+                                <label>Your message</label>
+                            </div>
+                        </div>
+                        <div className='text-end'>
+                            <button className='btn btn-secondary px-4' type="submit">
+                                Send <FontAwesomeIcon icon={faPaperPlane} />
+                            </button>
+                        </div>
                     </div>
-                    </div>
-                    <div className='text-end'>
-                    <button className='btn btn-secondary px-4' type="submit">
-                        Send <FontAwesomeIcon icon={faPaperPlane} />
-                    </button>
-                    </div>
-                </div>
                 </form>
             </div>
-            <div className='col-lg-12 row text-center'>
-                    <div className='col-3'>
-                        <a className='contact' target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/konner-hartman/"><FontAwesomeIcon icon={faLinkedin} /></a>
-                    </div>
-                    <div className='col-3'>
-                        <a className='contact' href="tel:214-226-4278"><FontAwesomeIcon icon={faMobileAlt} /></a>
-                    </div>
-                    <div className='col-3'>
-                        <a className='contact' target="_blank" rel="noreferrer" href="https://github.com/konnerhartman"><FontAwesomeIcon icon={faGithubAlt} /></a>
-                    </div>
-                    <div className='col-3'>
-                        <a className='contact' href="mailto:konner.hartman22@gmail.com"><FontAwesomeIcon icon={faEnvelope} /></a>
-                    </div>
-                </div>
         </div>
         <div className="col-1 d-none d-sm-block">
         </div>
