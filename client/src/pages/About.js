8e58c678ec64a3d226cb68c46c9528bb55baabc7
Konner Hartman <konner.hartman@yahoo.com>
2022-04-08 11:34:02 -0500

    updated about me page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import smileEmoji from '../assets/kh-emoji-smile.png';
 import resume from '../assets/K.-Hartman-Resume.pdf';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -16,8 +17,8 @@ const About = () => {
                 <div className="text-center col-lg-8">
                     <h2 className="fw-bold">About Me</h2>
                     <div>
-                        <p className='mt-4'>An innovative problem-solver passionate about user experience, my current focus is on app functionality and development.</p>
-                        <p className='mt-4'>I am a Full Stack Web Developer with an emphasis on the MERN stack and React, holding a Bachelor of Science From Texas A&M University. I recently received a certificate in Full Stack Development from Southern Methodist University, where I developed skills in JavaScript, MySQL, MongoDB, NodeJS, React.js, and more.</p>
+                        <p className='mt-4'>Constantly learning and loving every second of it.</p>
+                        <p className='m-4'>Being comfortable with discomfort is one of my greatest traits. I am a Full Stack Web Developer with an emphasis on the MERN stack, holding a Bachelor of Science From Texas A&M University. I recently received a certificate in Full Stack Development from Southern Methodist University, where I developed <Link className="text-white" to='/skills'>skills</Link> in JavaScript, MySQL, MongoDB, NodeJS, React.js, and more.</p>
                         <button type="button" className="aboutBtn btn btn-outline-light">
                             <a target="_blank" rel="noreferrer" className="nav-link active text-reset" href={resume}>
                                 <FontAwesomeIcon className="px-2" icon={faFile} />
